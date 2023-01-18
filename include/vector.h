29a9b680aab61c598ad0a626eab570e669183c4b
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2023-01-18 09:02:44 +0100

    Formatting code

--- include/vector.h
+++ include/vector.h
@@ -1,17 +1,17 @@
 //#pragma once
-#ifndef VECTOR_H
-#define VECTOR_H
+#ifndef __VECTOR_H
+#define __VECTOR_H
 
 typedef struct vector_t {
     size_t count;
-    void** items;
+    void **items;
 } vector_t;
 
-vector_t* vector_create();
-void vector_free(vector_t*);
+vector_t *vector_create();
+void vector_free(vector_t *);
 
-void vector_add(vector_t*, void*);
+void vector_add(vector_t *, void *);
 
-void* vector_remove(vector_t*, void*);
+void *vector_remove(vector_t *, void *);
 
-#endif // VECTOR_H
+#endif // __VECTOR_H
--- src/vector.c
+++ src/vector.c
@@ -1,50 +1,47 @@
 #include "vector.h"
 #include <stdlib.h>
 
-vector_t* vector_create() {
-    vector_t* result = (vector_t*)malloc(sizeof(vector_t));
+vector_t *vector_create() {
+    vector_t *result = (vector_t *)malloc(sizeof(vector_t));
     result->items = NULL;
     result->count = 0;
     return result;
 }
 
-void vector_free(vector_t* v) {
+void vector_free(vector_t *v) {
     free(v->items);
     v->items = NULL;
     v->count = 0;
     free(v);
 }
 
-void vector_add(vector_t* v, void* item) {
-    v->items = realloc(v->items, sizeof(void*) * (v->count + 1));
+void vector_add(vector_t *v, void *item) {
+    v->items = realloc(v->items, sizeof(void *) * (v->count + 1));
     v->items[v->count++] = item;
 }
 
-void* vector_remove(vector_t* v, void* item){
+void *vector_remove(vector_t *v, void *item) {
     size_t position;
     int found = 0;
-    for (size_t i = 0; i < v->count; i++)
-    {
-        void* current = v->items[i];
-        if(current == item){
+    for (size_t i = 0; i < v->count; i++) {
+        void *current = v->items[i];
+        if (current == item) {
             position = i;
             found = 1;
             break;
         }
     }
 
-    if (!found)
-    {
+    if (!found) {
         return NULL;
     }
-    
-    for (size_t i = position; i < v->count - 1; i++)
-    {
-        void* next = v->items[i+1];
+
+    for (size_t i = position; i < v->count - 1; i++) {
+        void *next = v->items[i + 1];
         v->items[i] = next;
     }
     v->items[v->count - 1] = NULL;
     v->count--;
-    
+
     return item;
 }

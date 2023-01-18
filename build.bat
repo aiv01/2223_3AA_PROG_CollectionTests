6049189e759017d3f567f0a89e6ac90cc326d6d2
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2023-01-18 08:37:38 +0100

    Reformat build.bat for make path indipendent

--- build.bat
+++ build.bat
@@ -1,9 +1,11 @@
 @echo off
 
-if exist "bin" (
-    RD /S /Q "bin"
+set BAT_BASE_PATH=%~p0
+
+if exist "%BAT_BASE_PATH%bin" (
+    RD /S /Q "%BAT_BASE_PATH%bin"
 )
 
-MD "bin"
+MD "%BAT_BASE_PATH%bin"
 
-clang -o bin\test.exe -Iinclude test\*.c src\*.c
+clang -o "%BAT_BASE_PATH%bin/test.exe" -I "%BAT_BASE_PATH%/include" "%BAT_BASE_PATH%test/*.c" "%BAT_BASE_PATH%src/*.c"
